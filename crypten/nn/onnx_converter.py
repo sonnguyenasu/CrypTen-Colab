287aa6af849c47f32933366f8fe945547150824d
Sơn Nguyễn <tson.csp@gmail.com>
2025-02-25 19:24:40 -0700

    Update onnx_converter.py

--- crypten/nn/onnx_converter.py
+++ crypten/nn/onnx_converter.py
@@ -12,6 +12,35 @@ import io
 import onnx
 import torch
 import torch.onnx.symbolic_helper as sym_help
+try:
+  import torch.onnx.symbolic_registry as sym_registry
+except:
+  import pytorch_pfn_extras
+  from typing import Callable
+
+  # if pytorch_pfn_extras.requires("1.13.0"):
+  import torch.onnx._internal.registration as reg
+  import torch.onnx.utils
+
+  def is_registered_op(op_type: str, domain: str, opset_version: int) -> bool:
+      return reg.registry.is_registered_op(f"{domain}::{op_type}", opset_version)
+
+  def get_registered_op(op_type: str, domain: str, opset_version: int) -> Callable:
+      group = reg.registry.get_function_group(f"{domain}::{op_type}")
+      assert group is not None
+      ret = group.get(opset_version)
+      assert ret is not None
+      return ret
+
+  def register_op(op_type: str, f: Callable, domain: str, opset_version: int) -> None:
+      if len(domain) == 0:
+          domain = "aten"
+      torch.onnx.utils.register_custom_op_symbolic(
+          f"{domain}::{op_type}", f, opset_version)
+  
+  from collections import namedtuple
+  Registry = namedtuple("Registry",["is_registered_op", "get_registered_op", "register_op"])
+  sym_registry = Registry(is_registered_op = is_registered_op, get_registered_op = get_registered_op, register_op=register_op)
 import torch.onnx.utils
 from onnx import numpy_helper
 from torch.onnx import OperatorExportTypes
@@ -19,24 +48,12 @@ from torch.onnx import OperatorExportTypes
 from . import module
 
 try:
-    import tensorflow as tf  # @manual
-    import tf2onnx  # @manual
+    import tensorflow as tf  # noqa
+    import tf2onnx
 
     TF_AND_TF2ONNX = True
 except ImportError:
     TF_AND_TF2ONNX = False
-
-try:
-    import torch.onnx.symbolic_registry as sym_registry  # noqa
-
-    SYM_REGISTRY = True
-except ImportError:
-    from torch.onnx._internal.registration import registry  # noqa
-
-    SYM_REGISTRY = False
-
-
-_OPSET_VERSION = 17
 
 
 def from_onnx(onnx_string_or_file):
@@ -141,7 +158,6 @@ def _export_pytorch_model(f, pytorch_model, dummy_input):
         "input_names": ["input"],
         "operator_export_type": OperatorExportTypes.ONNX,
         "output_names": ["output"],
-        "opset_version": _OPSET_VERSION,
     }
     torch.onnx.export(pytorch_model, dummy_input, f, **kwargs)
     return f
@@ -178,6 +194,7 @@ def _to_crypten(onnx_model):
 
     # loop over all nodes:
     for node in onnx_model.graph.node:
+
         # get attributes and node type:
         attributes = {attr.name: _get_attribute_value(attr) for attr in node.attribute}
         crypten_class = _get_operator_class(node.op_type, attributes)
@@ -265,40 +282,26 @@ def _update_onnx_symbolic_registry():
     Updates the ONNX symbolic registry for operators that need a CrypTen-specific
     implementation and custom operators.
     """
-    if SYM_REGISTRY:
-        # update PyTorch's symbolic ONNX registry to output different functions:
-        for version_key, version_val in sym_registry._registry.items():
-            for function_key in version_val.keys():
-                if function_key == "softmax":
-                    sym_registry._registry[version_key][function_key] = (
-                        _onnx_crypten_softmax
-                    )
-                if function_key == "log_softmax":
-                    sym_registry._registry[version_key][function_key] = (
-                        _onnx_crypten_logsoftmax
-                    )
-                if function_key == "dropout":
-                    sym_registry._registry[version_key][function_key] = (
-                        _onnx_crypten_dropout
-                    )
-                if function_key == "feature_dropout":
-                    sym_registry._registry[version_key][function_key] = (
-                        _onnx_crypten_feature_dropout
-                    )
-    else:
-        # Update ONNX symbolic registry using torch.onnx.register_custom_op_symbolic
-        torch.onnx.register_custom_op_symbolic(
-            "aten::softmax", _onnx_crypten_softmax, _OPSET_VERSION
-        )
-        torch.onnx.register_custom_op_symbolic(
-            "aten::log_softmax", _onnx_crypten_logsoftmax, _OPSET_VERSION
-        )
-        torch.onnx.register_custom_op_symbolic(
-            "aten::dropout", _onnx_crypten_dropout, _OPSET_VERSION
-        )
-        torch.onnx.register_custom_op_symbolic(
-            "aten::feature_dropout", _onnx_crypten_feature_dropout, _OPSET_VERSION
-        )
+
+    # update PyTorch's symbolic ONNX registry to output different functions:
+    for version_key, version_val in sym_registry._registry.items():
+        for function_key in version_val.keys():
+            if function_key == "softmax":
+                sym_registry._registry[version_key][
+                    function_key
+                ] = _onnx_crypten_softmax
+            if function_key == "log_softmax":
+                sym_registry._registry[version_key][
+                    function_key
+                ] = _onnx_crypten_logsoftmax
+            if function_key == "dropout":
+                sym_registry._registry[version_key][
+                    function_key
+                ] = _onnx_crypten_dropout
+            if function_key == "feature_dropout":
+                sym_registry._registry[version_key][
+                    function_key
+                ] = _onnx_crypten_feature_dropout
 
 
 @sym_help.parse_args("v", "i", "none")
